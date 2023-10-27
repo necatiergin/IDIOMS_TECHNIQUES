ac4121793e6060e21dc003bd301b93eaa2b71b71
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-27 09:11:14 +0300

    Create exchange_01.cpp

---
+++ exchange/exchange_01.cpp
@@ -0,0 +1,12 @@
+template<class T, class U = T>
+constexpr // Since C++20
+T exchange(T& obj, U&& new_value)
+noexcept( // Since C++23
+    std::is_nothrow_move_constructible<T>::value&&
+    std::is_nothrow_assignable<T&, U>::value
+    )
+{
+    T old_value = std::move(obj);
+    obj = std::forward<U>(new_value);
+    return old_value;
+}
