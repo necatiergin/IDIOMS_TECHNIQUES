d5b28d90aad33c9a77fdd1bafcb31a4428973e78
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-02 13:00:01 +0300

    Update strong_type_05.cpp

--- strong_types/strong_type_05.cpp
+++ strong_types/strong_type_05.cpp
@@ -1,15 +1,21 @@
 <!--
 #include <ostream>
 
-template <typename T, template<typename> class crtpType>
+
+template <typename T, template<typename> typename crtpType>
 struct crtp
 {
-    T& underlying() { return static_cast<T&>(*this); }
+    T& underlying() 
+    {
+        return static_cast<T&>(*this);
+    }
     T const& underlying() const
     {
         return static_cast<T const&>(*this);
     }
 };
+
+
 
 template <typename T>
 struct Addable : crtp<T, Addable>
@@ -48,30 +54,44 @@ struct Printable : crtp<T, Printable>
     }
 };
 
-template <typename T, typename Parameter>
-class NamedType : public Addable<NamedType<T, Parameter>>
+template <typename T, typename Parameter, template<typename> class... Skills>
+class NamedType : public Skills<NamedType<T, Parameter, Skills...>>...
 {
 public:
     explicit NamedType(T const& value) : value_(value) {}
-    T& get() { return value_; }
-    T const& get() const { return value_; }
+    T& get()
+    {
+        return value_;
+    }
+    T const& get() const
+    {
+        return value_;
+    }
 private:
     T value_;
 };
 
-template <typename T, typename Parameter>
-std::ostream& operator<<(std::ostream& os, NamedType<T, Parameter> const& object)
+
+template <typename T, typename Parameter, template<typename> class... Skills>
+std::ostream& operator<<(std::ostream& os, NamedType<T, Parameter, Skills...> const& object)
 {
     object.print(os);
     return os;
 }
 
-using Length = NamedType<double, struct LengthParameter>;
+using Length = NamedType<double, struct LengthParameter, Addable, Printable>;
+
+#include <iostream>
+
 
 int main()
 {
     Length lx{ 35.56 }, ly{ 56.24 };
 
-    auto result = lx + ly;
+    std::cout << lx + ly;
+
+
 }
+//The pick-and-choose interface
+
 -->
