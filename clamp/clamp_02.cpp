65c591f8ad3ea854174c40c33f9283c16e44487d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-04 06:30:36 +0300

    Create clamp_02.cpp

---
+++ clamp/clamp_02.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	array ar{ -5, 20, 10, 17, 3, -9, 0, 40, -9, 5};
+	for (auto i : ar)
+		cout << i << ' ';
+	cout << '\n';
+
+	int low, high;
+	
+	cout << "enter the range values: ";
+	cin >> low >> high;
+
+	transform(begin(ar), end(ar), begin(ar), [low, high](int x) {return clamp(x, low, high); });
+
+	for (auto i : ar)
+		cout << i << ' ';
+	cout << '\n';
+}
