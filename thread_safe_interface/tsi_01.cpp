ac5cdacc50fd9a1906c6e2c5ee0b9de1132cf69b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-01-06 05:52:49 +0300

    Update tsi_01.cpp

--- thread_safe_interface/tsi_01.cpp
+++ thread_safe_interface/tsi_01.cpp
@@ -1,1 +1,58 @@
-soon
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+class Tsi {
+public:
+    void foo()
+    {
+        std::scoped_lock lock(mtx);
+        impl_fx();
+    }
+
+    void bar()
+    {
+        std::scoped_lock lock(mtx);
+        impl_fy();
+        impl_fz();
+        impl_fx();
+    }
+
+private:
+
+    void impl_fx()
+    {
+        std::cout << "impl_fx: " << std::this_thread::get_id() << '\n';
+    }
+
+    void impl_fy()
+    {
+        std::cout << "impl_fy: " << std::this_thread::get_id() << '\n';
+    }
+    
+    void impl_fz()
+    {
+        std::cout << "impl_fz: " << std::this_thread::get_id() << '\n';
+    }
+
+    mutable std::mutex mtx;      
+};
+
+int main()
+{
+    using namespace std;
+    
+    Tsi tsi_object;
+
+    jthread t1([&] {
+        tsi_object.foo();
+        });
+
+    jthread t2([&] {
+        tsi_object.foo();
+        tsi_object.bar();
+        });
+
+    tsi_object.foo();
+    tsi_object.bar();
+}
