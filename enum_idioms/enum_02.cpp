70254ff5cf603a5f6ac7aade62e21c994ff3d2f3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-18 17:12:59 +0300

    Create enum_02.cpp

---
+++ enum_idioms/enum_02.cpp
@@ -0,0 +1,30 @@
+#include <cstdint>
+#include <iostream>
+#include <type_traits>
+#include <concepts>
+
+enum class Fruit : uint8_t
+{
+    Apple,
+    Banana,
+    Orange,
+    Grape,
+    Mango,
+};
+
+
+template <typename T>
+concept enumtype = std::is_enum_v<T>;
+
+template <enumtype T>
+auto get_enum_value(const T& e)
+{
+    return static_cast<std::underlying_type_t<T>>(e);
+}
+
+int main()
+{
+    Fruit f { Fruit::Grape};
+
+    std::cout << "Enum value: " << +get_enum_value(f) << '\n';
+}
