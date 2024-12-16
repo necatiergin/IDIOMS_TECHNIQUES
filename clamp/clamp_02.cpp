a0b02187af783f5cde96e46aade9dee400511ec2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-16 23:35:32 +0300

    Update clamp_02.cpp

--- clamp/clamp_02.cpp
+++ clamp/clamp_02.cpp
@@ -4,21 +4,20 @@
 
 int main()
 {
-	using namespace std;
-
-	array ar{ -5, 20, 10, 17, 3, -9, 0, 40, -9, 5};
-	for (const int  i : ar)
-		cout << i << ' ';
-	cout << '\n';
+	std::array ar{ -5, 20, 10, 17, 3, -9, 0, 40, -9, 5 };
+	for (const int i : ar)
+		std::cout << i << ' ';
+	
+	std::cout << '\n';
 
 	int low, high;
-	
-	cout << "enter the range values: ";
-	cin >> low >> high;
 
-	transform(begin(ar), end(ar), begin(ar), [low, high](int x) {return clamp(x, low, high); });
+	std::cout << "enter the range values: ";
+	std::cin >> low >> high;
+
+	transform(begin(ar), end(ar), begin(ar), [low, high](int x) {return std::clamp(x, low, high); });
 
 	for (const int i : ar)
-		cout << i << ' ';
-	cout << '\n';
+		std::cout << i << ' ';
+	std::cout << '\n';
 }
