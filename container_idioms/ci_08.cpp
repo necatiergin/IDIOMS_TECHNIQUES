f0ce9c6a43c3d26d860830794f4fcfa4c38c1eaa
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-18 07:54:13 +0300

    Create ci_08.cpp

---
+++ container_idioms/ci_08.cpp
@@ -0,0 +1,32 @@
+//remove-erase idiom
+
+#include <vector>
+#include <iostream>
+#include <random>
+#include <algorithm>
+
+void test_fill(std::vector<int>& vec)
+{
+	static std::mt19937 eng{ std::random_device{}() };
+	static std::uniform_int_distribution dist{ 0, 9};
+	std::generate(vec.begin(), vec.end(), [] {return dist(eng); });
+}
+
+int main()
+{
+	std::vector<int> vec(50);
+	test_fill(vec);
+	const auto print = [&vec] {
+		for (int i : vec) std::cout << i << ' ';
+		std::cout << '\n';
+		};
+
+	print();
+	std::cout << "enter the value to erase: ";
+	int val;
+	std::cin >> val;
+
+	auto n = std::erase(vec, val);
+	std::cout << n << " elem. erased.\n";
+	print();
+}
