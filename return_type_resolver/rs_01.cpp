66b00ecacb5848a20bdc2db96798f569cde3248e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-18 10:33:22 +0300

    Update and rename resolver_02.cpp to rs_01.cpp

--- return_type_resolver/rs_01.cpp
+++ return_type_resolver/rs_01.cpp
@@ -7,6 +7,9 @@ public:
     String(const std::string& s) : m_str{ s } {}
     operator int() const { return std::stoi(m_str); }
     operator double() const { return std::stod(m_str); }
+    operator long() const { return std::stol(m_str); }
+    operator unsigned long() const { return std::stoul(m_str); }
+    operator unsigned long long() const { return std::stoull(m_str); }
     //...
 };
 
@@ -14,6 +17,6 @@ int main()
 {
     int ival = String("654");
     double dval = String("9.752");
-    std::cout << "ival = " << ival << "\n";
-    std::cout << "dval = " << dval << "\n";
+    std::cout << "ival = " << ival << '\n';
+    std::cout << "dval = " << dval << '\n';
 }
