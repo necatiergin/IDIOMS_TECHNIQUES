919c2d886615c52ba6b17d3ce0752ec3415d2c8a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-09-12 14:59:26 +0300

    Update stream.cpp

--- nifty_counter/stream.cpp
+++ nifty_counter/stream.cpp
@@ -1,31 +1,46 @@
-// Stream.cpp
-//#include "Stream.h"
+#include "stream.h"
+#include <cstddef> // std::byte
+#include <new>     // placement-new
 
-#include <new>         // placement new
-#include <type_traits> // aligned_storage
+// TU-local counter: initialized to 0 at program load time
 
-static int nifty_counter; // zero initialized at load time
-//static typename std::aligned_storage<sizeof(Stream), alignof (Stream)>::type stream_buf; // memory for the stream object
-alignas(Stream) std::byte stream_buf[sizeof(Stream)];
+static int nifty_counter = 0;
 
-Stream& gstream = reinterpret_cast<Stream&> (stream_buf);
+// aligned storage for stream object
+alignas(Stream) static std::byte stream_buf[sizeof(Stream)];
 
-Stream::Stream()
+// definition of Stream& gstream declared as extern in the header stream.h
+Stream& gstream = reinterpret_cast<Stream&>(stream_buf);
+
+// --- Stream members ---
+Stream::Stream() 
 {
-	// initialize things
-}
-Stream::~Stream()
-{
-	// clean-up
-}
-
-StreamInitializer::StreamInitializer()
-{
-	if (nifty_counter++ == 0) new (&stream) Stream(); // placement new
-}
-StreamInitializer::~StreamInitializer()
-{
-	if (--nifty_counter == 0) (&stream)->~Stream();
+    std::cout << "[Stream::ctor]\n";
 }
 
+Stream::~Stream() 
+{
+    std::cout << "[Stream::dtor]\n";
+}
 
+void Stream::write(const char* msg) 
+{
+    std::cout << "[Stream] " << msg << '\n';
+}
+
+// --- Nifty guard ---
+StreamInitializer::StreamInitializer() 
+{
+    if (nifty_counter++ == 0) {
+        // first guard constructs the object with placement new
+        new (&gstream) Stream();
+    }
+}
+
+StreamInitializer::~StreamInitializer() 
+{
+    if (--nifty_counter == 0) {
+        // last guard calls the destructor of the srvice object
+        (&gstream)->~Stream();
+    }
+}
